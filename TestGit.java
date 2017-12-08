4cfea0eed03954a8dd6031563d81133e446158db
aish92 <aish.5892@gmail.com>
2017-12-08 10:56:06 -0800

    Update TestGit.java

--- TestGit.java
+++ TestGit.java
@@ -3,7 +3,7 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("margie Test for Git Commit");
+		System.out.println("testing code maat churn summary");
 	}
 
 }
